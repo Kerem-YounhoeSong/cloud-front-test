3a592a6d08e05637a459fed461b5732815715e50
Kerem-YounhoeSong <yhsong@lunasoft.co.kr>
2021-04-19 15:11:13 +0900

    feat: cloud-front-test

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Hello world!</div>
+      <div>cloud front test by Kerem</div>
+    </div>
+  );
+}
+
+export default App;
